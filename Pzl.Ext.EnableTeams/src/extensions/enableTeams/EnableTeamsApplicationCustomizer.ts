0f1a4a249ca76c4a494554dd440dcd35036c9cb7
Mikael Svenson <miksvenson@gmail.com>
2018-04-06 11:51:02 +0200

    Added polyfill for Array.find

--- Pzl.Ext.EnableTeams/src/extensions/enableTeams/EnableTeamsApplicationCustomizer.ts
+++ Pzl.Ext.EnableTeams/src/extensions/enableTeams/EnableTeamsApplicationCustomizer.ts
@@ -29,6 +29,8 @@ export default class EnableTeamsApplicationCustomizer
             let autoCreate = this.properties.autoCreate.toString().toLowerCase() === 'true';
             this.DoWork(autoCreate);
         }
+
+        this.arrayPolyfill();        
         return Promise.resolve();
     }
 
@@ -71,5 +73,54 @@ export default class EnableTeamsApplicationCustomizer
                 ReactDOM.render(React.createElement(TeamsButton, buttonProps, null), this._topPlaceholder.domElement);
             }
         }
+    }
+
+    private arrayPolyfill()
+    {
+        if (!(<any>Array.prototype).find) {
+            Object.defineProperty(Array.prototype, 'find', {
+              value: function(predicate) {
+               // 1. Let O be ? ToObject(this value).
+                if (this == null) {
+                  throw new TypeError('"this" is null or not defined');
+                }
+          
+                var o = Object(this);
+          
+                // 2. Let len be ? ToLength(? Get(O, "length")).
+                var len = o.length >>> 0;
+          
+                // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+                if (typeof predicate !== 'function') {
+                  throw new TypeError('predicate must be a function');
+                }
+          
+                // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+                var thisArg = arguments[1];
+          
+                // 5. Let k be 0.
+                var k = 0;
+          
+                // 6. Repeat, while k < len
+                while (k < len) {
+                  // a. Let Pk be ! ToString(k).
+                  // b. Let kValue be ? Get(O, Pk).
+                  // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+                  // d. If testResult is true, return kValue.
+                  var kValue = o[k];
+                  if (predicate.call(thisArg, kValue, k, o)) {
+                    return kValue;
+                  }
+                  // e. Increase k by 1.
+                  k++;
+                }
+          
+                // 7. Return undefined.
+                return undefined;
+              },
+              configurable: true,
+              writable: true
+            });
+          }
     }
 }
