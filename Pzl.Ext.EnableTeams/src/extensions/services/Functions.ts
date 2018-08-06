58c5787c9db6a33a4fd6077e38c3d70c61313c0c
Mikael Svenson <miksvenson@gmail.com>
2018-08-06 15:49:24 +0200

    Bugfixes for IE11 Bugfix if redirectProp was not specified Added default payload

--- Pzl.Ext.EnableTeams/src/extensions/services/Functions.ts
+++ Pzl.Ext.EnableTeams/src/extensions/services/Functions.ts
@@ -5,20 +5,34 @@ import { Web, Site } from '@pnp/sp';
 export class Functions {
     public static async CreateTeam(graphHttpClient: GraphHttpClient, groupId: string, siteUrl: string) {
         console.log("Creating team");
-        await MSGraph.Put(graphHttpClient, `beta/groups/${groupId}/team`, "{}");
+        let payload: any = {
+            "memberSettings": {
+                "allowCreateUpdateChannels": true
+            },
+            "messagingSettings": {
+                "allowUserEditMessages": true,
+                "allowUserDeleteMessages": true
+            },
+            "funSettings": {
+                "allowGiphy": true,
+                "giphyContentRating": "strict"
+            }
+        };
+        await MSGraph.Put(graphHttpClient, `beta/groups/${groupId}/team`, JSON.stringify(payload));
         let teamsUri;
         while (true) {
             let endPointInfo = await MSGraph.Get(graphHttpClient, `beta/groups/${groupId}/endpoints`);
             if (endPointInfo && endPointInfo.value && endPointInfo.value.length > 0) {
                 let info = endPointInfo.value.find(element => { return element.providerName === 'Microsoft Teams'; });
                 if (info) {
+                    console.log("Adding teams link");
                     teamsUri = info.uri;
                     let currentWeb = new Web(siteUrl);
                     await currentWeb.navigation.quicklaunch.add("Teams", info.uri);
-                    console.log("Adding teams link");
                     break;
                 }
             } else {
+                console.log("Waiting for teams to be ready");
                 await this.Timeout(500);
             }
         }
