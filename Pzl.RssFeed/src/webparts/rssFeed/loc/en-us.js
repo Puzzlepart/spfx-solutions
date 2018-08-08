7457e70bfe50d9e287c9a77745c6b0b1a95b9e39
Thomas Granheim <Thomasog@puzzlepart.com>
2018-08-08 12:27:31 +0200

    Pzl.RssFeed: Initial commit

---
+++ Pzl.RssFeed/src/webparts/rssFeed/loc/en-us.js
@@ -0,0 +1,14 @@
+define([], function () {
+  return {
+    "PropertyPaneDescription": "",
+    "GeneralGroupName": "",
+    "RssFeedUrlFieldLabel": "URL",
+    "Rss2jsonApiKeyFieldLabel": "API Key (rss2json)",
+    "ItemsCountFieldLabel": "Number of elements",
+    "IconLabel": "Icon",
+    "CacheExpirationTimeFieldLabel": "Cache duration",
+    "View_EmptyPlaceholder_Label": 'Edit Web Part',
+    "View_EmptyPlaceholder_Description": "Property 'List' is not yet configured.",
+    "View_EmptyPlaceholder_Button": 'Edit',
+  }
+});
---
+++ Pzl.RssFeed/src/webparts/rssFeed/loc/nb-no.js
@@ -0,0 +1,14 @@
+define([], function () {
+    return {
+        "PropertyPaneDescription": "",
+        "GeneralGroupName": "",
+        "RssFeedUrlFieldLabel": "URL",
+        "Rss2jsonApiKeyFieldLabel": "API Key (rss2json)",
+        "ItemsCountFieldLabel": "Antall elementer",
+        "IconLabel": "Ikon",
+        "CacheExpirationTimeFieldLabel": "Cache varighet (minutter)",
+        "View_EmptyPlaceholder_Label": 'Edit Web Part',
+        "View_EmptyPlaceholder_Description": "Property 'List' is not yet configured.",
+        "View_EmptyPlaceholder_Button": 'Edit',
+    }
+});
