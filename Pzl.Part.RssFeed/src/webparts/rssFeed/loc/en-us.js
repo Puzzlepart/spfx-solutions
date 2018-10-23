2819537399f237309387eb81610f7dd4e73e9d3b
Tarald <tarald.gasbakk@puzzlepart.com>
2018-10-23 08:41:38 +0200

    Created "See All" Button on RSS - Feed.

--- Pzl.Part.RssFeed/src/webparts/rssFeed/loc/en-us.js
+++ Pzl.Part.RssFeed/src/webparts/rssFeed/loc/en-us.js
@@ -6,10 +6,12 @@ define([], function () {
     "Rss2jsonApiKeyFieldLabel": "API Key (rss2json)",
     "ItemsCountFieldLabel": "Number of elements",
     "IconLabel": "Icon",
-    "CacheExpirationTimeFieldLabel": "Cache duration",
+    "CacheExpirationTimeFieldLabel": "Cache duration (minutes)",
+    "CacheExpirationTimeFieldDescription": 'How many minutes the browser should save (cache) the results from the RSS - API.',
     "View_PublishLabel": "Published",
     "View_EmptyPlaceholder_Label": 'Edit Web Part',
     "View_EmptyPlaceholder_Description": "Property 'RSS-Feed URL' is not yet configured.",
     "View_EmptyPlaceholder_Button": 'Edit',
+    "SeeAllUrlFieldLabel": '"View All" - link',
   }
 });
