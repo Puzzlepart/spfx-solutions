64f406bce480e16440dc2c2daebd1a082c9bcd15
Mikael Svenson <miksvenson@gmail.com>
2018-11-02 14:15:08 +0100

    Made as global extension. Embed icon as svg.

--- Pzl.Ext.PDFExport/gulpfile.js
+++ Pzl.Ext.PDFExport/gulpfile.js
@@ -2,6 +2,14 @@
 
 const gulp = require('gulp');
 const build = require('@microsoft/sp-build-web');
+
+let copyIcons = build.subTask('copy-icons', function (gulp, buildOptions, done) {
+    gulp.src('./*.svg')
+        .pipe(gulp.dest('./temp/deploy'));
+    done();
+});
+build.rig.addPostBuildTask(copyIcons);
+
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
 build.initialize(gulp);
