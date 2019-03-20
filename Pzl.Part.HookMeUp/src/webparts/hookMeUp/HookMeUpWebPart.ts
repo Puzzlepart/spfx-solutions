b7ed81e242713dcdbc437d6d44ba09b055d5fe28
Mikael Svenson <miksvenson@gmail.com>
2019-03-20 09:03:43 +0100

    Initial commit

---
+++ Pzl.Part.HookMeUp/src/webparts/hookMeUp/HookMeUpWebPart.ts
@@ -0,0 +1,69 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+    BaseClientSideWebPart,
+    IPropertyPaneConfiguration,
+    PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+import PropertyPaneLogo from './PropertyPaneLogo';
+import { DisplayMode } from '@microsoft/sp-core-library';
+import * as strings from 'HookMeUpWebPartStrings';
+
+export interface IHookMeUpWebPartProps {
+    anchorId: string;
+}
+
+export default class HookMeUpWebPart extends BaseClientSideWebPart<IHookMeUpWebPartProps> {
+
+    public render(): void {
+        if (this.displayMode === DisplayMode.Edit) {
+            this.domElement.innerHTML = `
+                <div id="${this.properties.anchorId}" style="margin-bottom:50px">
+                    <b>Anchor:</b> ${this.properties.anchorId} <i>(${strings.NotVisible})</i>
+                </div>
+            `;
+        } else {
+            let element: HTMLElement = this.domElement.parentElement;
+            // check up to 5 levels up for padding
+            for (let i: number = 0; i < 5; i++) {
+                const style: CSSStyleDeclaration = window.getComputedStyle(element);
+                const hasPadding: boolean = style.paddingTop !== '0px';
+                if (hasPadding) {
+                    element.style.paddingTop = '0';
+                    element.style.paddingBottom = '0';
+                    element.style.marginTop = '0';
+                    element.style.marginBottom = '0';
+                }
+                if (element.className === 'ControlZone') {
+                    break;
+                }
+                element = element.parentElement;
+            }
+            this.domElement.innerHTML = this.domElement.innerHTML = `<span id="${this.properties.anchorId}"></span>`;
+        }
+    }
+
+    protected get dataVersion(): Version {
+        return Version.parse('1.0');
+    }
+
+    protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+        return {
+            pages: [
+                {
+                    groups: [
+                        {
+                            groupFields: [
+                                PropertyPaneTextField('anchorId', {
+                                    label: strings.AnchorId,
+                                    description: strings.Description,
+                                    value: this.properties.anchorId
+                                }),
+                                new PropertyPaneLogo()
+                            ]
+                        }
+                    ]
+                }
+            ]
+        };
+    }
+}
---
+++ Pzl.Part.HookMeUp/src/webparts/hookMeUp/loc/en-us.js
@@ -0,0 +1,7 @@
+define([], function () {
+    return {
+        "AnchorId": "Anchor Id",
+        "Description": 'Unique id of the anchor to reference. Use letters and characters only. Eg. hook-me-up, which is referenced with href="#hook-me-up".',
+        "NotVisible": "only visible in edit mode"
+    }
+});
