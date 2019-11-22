52701e863d54c81095c82cec36ad45991e187d77
Thomas Granheim <Thomasog@puzzlepart.com>
2019-11-22 10:03:59 +0100

    Set image since it wont propagate from exchange /_api/groupservice/setgroupimage

--- Pzl.Ext.GroupLogo/src/extensions/groupLogo/GroupLogoApplicationCustomizer.ts
+++ Pzl.Ext.GroupLogo/src/extensions/groupLogo/GroupLogoApplicationCustomizer.ts
@@ -3,10 +3,9 @@ import { Log } from '@microsoft/sp-core-library';
 import {
     BaseApplicationCustomizer
 } from '@microsoft/sp-application-base';
-import { SPHttpClient } from '@microsoft/sp-http';
-import { Web, Site } from '@pnp/sp';
+import { SPHttpClient, MSGraphClient } from '@microsoft/sp-http';
+import { sp, Web, Site } from '@pnp/sp';
 import { Dialog } from '@microsoft/sp-dialog';
-import { MSGraph } from '../services';
 
 import * as strings from 'GroupLogoApplicationCustomizerStrings';
 
@@ -18,14 +17,21 @@ export interface IGroupLogoApplicationCustomizerProperties {
 /** A Custom Action which can be run during execution of a Client Side Application */
 export default class GroupLogoApplicationCustomizer
     extends BaseApplicationCustomizer<IGroupLogoApplicationCustomizerProperties> {
-
     @override
     public onInit(): Promise<void> {
-        if (typeof console == "undefined" || typeof console.log == "undefined") var console = { log: function () { } };
-        this.context.placeholderProvider.changedEvent.add(this, () => { this.DoWork(this.properties.logoUrl) });
+        if (typeof console == "undefined" || typeof console.log == "undefined") var console = { log: () => { } };
+        this.context.placeholderProvider.changedEvent.add(this, () => { this.DoWork(this.properties.logoUrl); });
         return Promise.resolve();
     }
 
+    /**
+     *
+     *
+     * @private
+     * @param {string} logoUrl
+     * @returns
+     * @memberof GroupLogoApplicationCustomizer
+     */
     private async DoWork(logoUrl: string) {
         let isGroupOwner = this.context.pageContext.legacyPageContext.isSiteAdmin;
         if (!isGroupOwner) return;
@@ -33,37 +39,47 @@ export default class GroupLogoApplicationCustomizer
         let response = await this.context.spHttpClient.post(`${logoUrl}/_api/contextinfo`, SPHttpClient.configurations.v1, {});
         if (!response.ok) return;
         let result = await response.json();
+
         // get web url from full path
         let webUrl = result.WebFullUrl;
-
         let web: Web = new Web(webUrl);
         let replace = `${window.location.protocol}//${window.location.hostname}`;
-        let relativeUrlLogo = logoUrl.replace(replace, "");
+        let relativeUrlLogo = decodeURIComponent(logoUrl).replace(replace, "");
         let buffer = await web.getFileByServerRelativeUrl(relativeUrlLogo).getBuffer();
 
+        // Url wont propagate from exchange, so also setting it directly on group
+        this.setGroupLogo(buffer);
+        console.log(relativeUrlLogo);
         let hasError = false;
         let groupId = this.context.pageContext.legacyPageContext.groupId;
 
         try {
             Dialog.alert(strings.SettingUp);
-            await MSGraph.Patch(this.context.graphHttpClient, `v1.0/groups/${groupId}/photo/$value`, buffer);
-            console.log("Logo updated in the graph");
+
+            let graphUrl = `/groups/${groupId}/photo`;
+            let graphClient: MSGraphClient = await this.context.msGraphClientFactory.getClient();
+            let caller = await graphClient.api(graphUrl).version("beta").header("Content-Type", "image/jpeg").put(buffer);
         } catch (err) {
             // Most likely due to user not having Exchange Online license or Group not ready
-            console.log(err);
             hasError = true;
+            throw err;
         }
 
         if (!hasError) {
             window.setTimeout(async () => {
                 let currentWeb = new Web(this.context.pageContext.web.absoluteUrl);
-                await currentWeb.getFolderByServerRelativeUrl(`${this.context.pageContext.web.serverRelativeUrl}/SiteAssets/__siteIcon__.jpg`).delete();
-                console.log("Remove site icon file - to force update");
-                this.removeCustomizer();
+                 await currentWeb.getFolderByServerRelativeUrl(`${this.context.pageContext.web.serverRelativeUrl}/SiteAssets/__siteIcon__.jpg`).delete();
+                // this.removeCustomizer();
             }, 3000);
         }
     }
 
+    /**
+     *
+     *
+     * @private
+     * @memberof GroupLogoApplicationCustomizer
+     */
     private async removeCustomizer() {
         // Remove custom action from current sute
         let site = new Site(this.context.pageContext.site.absoluteUrl);
@@ -75,6 +91,23 @@ export default class GroupLogoApplicationCustomizer
                 console.log("Logo extension removed");
                 break;
             }
+        }
+    }
+
+    
+    /**
+     *
+     *
+     * @private
+     * @param {*} fileBuffer
+     * @memberof GroupLogoApplicationCustomizer
+     */
+    private async  setGroupLogo(fileBuffer: ArrayBuffer) {
+        try {
+            let requestUrl = this.context.pageContext.web.serverRelativeUrl +  "/_api/groupservice/setgroupimage";
+           await this.context.spHttpClient.post(requestUrl, SPHttpClient.configurations.v1, {body: fileBuffer, headers: { 'Content-Type': 'image/png' } });
+        } catch (error) {
+            throw error;
         }
     }
 }
--- Pzl.Ext.GroupLogo/src/extensions/services/MSGraph.ts
+++ Pzl.Ext.GroupLogo/src/extensions/services/MSGraph.ts
@@ -1,25 +1,25 @@
-import { GraphHttpClient, GraphHttpClientResponse } from '@microsoft/sp-http';
-import { Log } from '@microsoft/sp-core-library';
+// import { GraphHttpClient, GraphHttpClientResponse } from '@microsoft/sp-http';
+// import { Log } from '@microsoft/sp-core-library';
 
-export class MSGraph {
-    public static async Patch(graphClient: GraphHttpClient, url: string, payload: any): Promise<boolean> {
-        const requestHeaders: Headers = new Headers();
-        requestHeaders.append("Content-Type", "image/jpeg");
+// export class MSGraph {
+//     public static async Patch(graphClient: GraphHttpClient, url: string, payload: any): Promise<boolean> {
+//         const requestHeaders: Headers = new Headers();
+//         requestHeaders.append("Content-Type", "image/jpeg");
         
-        let response: GraphHttpClientResponse = await graphClient.fetch(url, GraphHttpClient.configurations.v1, {
-            body: payload,
-            method: "PATCH",
-            headers: requestHeaders
-        });
-        // Check that the request was successful
-        if (response.ok) {
-            return true;
-        }
-        else {
-            // Reject with the error message
-            let error = new Error(response.statusText);
-            Log.error("Graph call", error);
-            throw error;
-        }
-    }
-}
+//         let response: GraphHttpClientResponse = await graphClient.fetch(url, GraphHttpClient.configurations.v1, {
+//             body: payload,
+//             method: "PATCH",
+//             headers: requestHeaders
+//         });
+//         // Check that the request was successful
+//         if (response.ok) {
+//             return true;
+//         }
+//         else {
+//             // Reject with the error message
+//             let error = new Error(response.statusText);
+//             Log.error("Graph call", error);
+//             throw error;
+//         }
+//     }
+// }
