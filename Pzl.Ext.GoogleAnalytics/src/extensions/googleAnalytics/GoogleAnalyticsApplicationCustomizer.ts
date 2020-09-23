69852fe184cda81e16446628ff540b0e54388bde
Peter Holme Obrestad <peter.holme.obrestad@nav.no>
2020-09-23 21:55:39 +0200

    Fix missing pageviews in gtm

--- Pzl.Ext.GoogleAnalytics/src/extensions/googleAnalytics/GoogleAnalyticsApplicationCustomizer.ts
+++ Pzl.Ext.GoogleAnalytics/src/extensions/googleAnalytics/GoogleAnalyticsApplicationCustomizer.ts
@@ -15,68 +15,45 @@ export default class GoogleAnalyticsApplicationCustomizer
   private currentPage = "";
   private isInitialLoad = true;
 
-  private getFreshCurrentPage(): string {
-    return window.location.pathname + window.location.search;
-  }
-  private updateCurrentPage(): void {
-    this.currentPage = this.getFreshCurrentPage();
-  }
-
-  private navigatedEvent(): void {
-    const navigatedPage = this.getFreshCurrentPage();
-    if (!this.isInitialLoad && (navigatedPage !== this.currentPage)) {
-      this.realNavigatedEvent();
-      this.updateCurrentPage();
-    }
-    this.isInitialLoad = false;
-  }
-
-  private realNavigatedEvent(): void {
-    this.track();
-  }
   /**
-   *
-   *
    * @returns {Promise<void>}
    * @memberof GoogleAnalyticsApplicationCustomizer
    */
   @override
   public onInit(): Promise<void> {
-    this.track();
+    this._track();
     this.context.application.navigatedEvent.add(this, () => {
-      this.navigatedEvent();
+      this._navigatedEvent();
     });
     return Promise.resolve();
   }
 
-  /**
-   *
-   *
-   * @private
-   * @memberof GoogleAnalyticsApplicationCustomizer
-   */
-  private track(): void {
+  private _getFreshCurrentPage(): string {
+    return window.location.pathname + window.location.search;
+  }
+
+  private _navigatedEvent(): void {
+    const navigatedPage = this._getFreshCurrentPage();
+    if (!this.isInitialLoad && (navigatedPage !== this.currentPage)) {
+      this.currentPage = this._getFreshCurrentPage();
+      this._track();
+    }
+    this.isInitialLoad = false;
+  }
+
+  private _track(): void {
     let trackerID: string = this.properties.trackerID;
     if (trackerID) {
       if (trackerID.indexOf("GTM-") === 0) {
-        let iframe: any = document.createElement("iframe");
-        iframe.id = "GoogleAnalyticsApplicationCustomizerIframe";
-        iframe.height = "0";
-        iframe.width = "0";
-        iframe.style = "display:none;visibility:hidden";
-        iframe.src = `https://www.googletagmanager.com/ns.html?id=${trackerID}`;
-
-        let element = document.getElementById("GoogleAnalyticsApplicationCustomizerIframe");
-        if (typeof (element) !== 'undefined' && element !== null) {
-          document.body.removeChild(element);
+        if (this.isInitialLoad) {
+          eval(`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${trackerID}');`);
+        } else {
+          eval(`dataLayer.push({event: 'pageview'});`);
         }
-        document.body.appendChild(iframe);
-        eval(`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${trackerID}');
-        `);
         Log.info(LOG_SOURCE, `Tracking with ID ${trackerID}`);
       } else if (trackerID.indexOf("UA-") === 0) {
         eval(`(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
