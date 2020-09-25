0d155335957c66f19ec154f436d727158871670a
Peter Holme Obrestad <peter.holme.obrestad@nav.no>
2020-09-25 15:20:15 +0200

    Refactor and simplify code

--- Pzl.Ext.GoogleAnalytics/src/extensions/googleAnalytics/GoogleAnalyticsApplicationCustomizer.ts
+++ Pzl.Ext.GoogleAnalytics/src/extensions/googleAnalytics/GoogleAnalyticsApplicationCustomizer.ts
@@ -12,60 +12,77 @@ export interface IGoogeAnalyticsApplicationCustomizerProperties {
 export default class GoogleAnalyticsApplicationCustomizer
   extends BaseApplicationCustomizer<IGoogeAnalyticsApplicationCustomizerProperties> {
 
-  private currentPage = "";
-  private isInitialLoad = true;
-
   /**
    * @returns {Promise<void>}
    * @memberof GoogleAnalyticsApplicationCustomizer
    */
   @override
   public onInit(): Promise<void> {
-    this._track();
-    this.context.application.navigatedEvent.add(this, () => {
-      this._navigatedEvent();
-    });
+    // Is this a virtual pageload?
+    if (!(window as any).isNavigatedEventSubscribed) {
+      // This is an initial pageload. Attach tracking to navigatedEvent
+      this.context.application.navigatedEvent.add(this, this._navigatedEvent);
+      (window as any).isNavigatedEventSubscribed = true;
+    }
     return Promise.resolve();
   }
 
-  private _getFreshCurrentPage(): string {
-    return window.location.pathname + window.location.search;
+  @override
+  public onDispose(): Promise<void> {
+    this.context.application.navigatedEvent.remove(this, this._navigatedEvent);
+    (window as any).isNavigatedEventSubscribed = false;
+    (window as any).currentPage = '';
+    return Promise.resolve();
   }
 
   private _navigatedEvent(): void {
-    const navigatedPage = this._getFreshCurrentPage();
-    if (!this.isInitialLoad && (navigatedPage !== this.currentPage)) {
-      this.currentPage = this._getFreshCurrentPage();
-      this._track();
-    }
-    this.isInitialLoad = false;
+    // Timeout for possibility to delay script loading. Avoid if possible.
+    setTimeout(() => {
+      // Only track if the URL has changed
+      if ((window as any).currentPage !== this._getCurrentPage()) {
+        this._track();
+        (window as any).currentPage = this._getCurrentPage();
+      }
+    }, 0);
   }
 
-  private _track(): void {
-    let trackerID: string = this.properties.trackerID;
-    if (trackerID) {
-      if (trackerID.indexOf("GTM-") === 0) {
-        if (this.isInitialLoad) {
-          eval(`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${trackerID}');`);
-        } else {
-          eval(`dataLayer.push({event: 'pageview'});`);
-        }
-        Log.info(LOG_SOURCE, `Tracking with ID ${trackerID}`);
-      } else if (trackerID.indexOf("UA-") === 0) {
+  private _track() {
+    const trackerID: string = this.properties.trackerID;
+    // Do we have valid-isch tracking code?
+    if (trackerID && trackerID.indexOf("GTM-") === 0) {
+      // Is GTM instantiated?
+      const dataLayerName = 'dataLayer';
+      if (typeof((window as any).dataLayer) === "object") {
+        // Use the existing GTM dataLayer object to track pageview
+        eval(`${dataLayerName}.push({event: 'pageview'});`);
+      } else {
+        // Instantiate GTM (which will also trigger a pageview)
+        eval(`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='${dataLayerName}'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','${dataLayerName}','${trackerID}');`);
+      }
+      Log.info(LOG_SOURCE, `Tracking with ID ${trackerID}`);
+    } else if (trackerID.indexOf("UA-") === 0) {
+      const gaName = 'ga';
+      if (typeof((window as any).ga) === "object") {
+        eval(`${gaName}('send', 'pageview');`);
+      } else {
         eval(`(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-          ga('create', '${trackerID}', 'auto');
-          ga('send', 'pageview');`);
-        Log.info(LOG_SOURCE, `Tracking with ID ${trackerID}`);
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','${gaName}');
+          ${gaName}('create', '${trackerID}', 'auto');
+          ${gaName}('send', 'pageview');`);
       }
+      Log.info(LOG_SOURCE, `Tracking with ID ${trackerID}`);
     } else {
       Log.info(LOG_SOURCE, "Tracking ID not provided");
     }
+  }
+
+  private _getCurrentPage(): string {
+    return window.location.pathname + window.location.search;
   }
 }
