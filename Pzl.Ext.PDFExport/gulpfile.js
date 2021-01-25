a91a0491edada8d70b1ddeda4080002f2893a3c6
Mikael Svenson <misvenso@microsoft.com>
2021-01-25 14:23:53 +0100

    Optimized stats build

--- Pzl.Ext.PDFExport/gulpfile.js
+++ Pzl.Ext.PDFExport/gulpfile.js
@@ -4,7 +4,7 @@ const gulp = require('gulp');
 const path = require('path');
 const build = require('@microsoft/sp-build-web');
 
-let copyIcons = build.subTask('copy-icons', function (gulp, buildOptions, done) {
+let copyIcons = build.subTask('copy-icons', function(gulp, buildOptions, done) {
     gulp.src('./*.svg')
         .pipe(gulp.dest('./temp/deploy'));
     done();
@@ -14,20 +14,19 @@ build.rig.addPostBuildTask(copyIcons);
 const bundleAnalyzer = require('webpack-bundle-analyzer');
 
 build.configureWebpack.mergeConfig({
-  additionalConfiguration: (generatedConfiguration) => {
-    const lastDirName = path.basename(__dirname);
-    const dropPath = path.join(__dirname, 'temp', 'stats');
-    generatedConfiguration.plugins.push(new bundleAnalyzer.BundleAnalyzerPlugin({
-      openAnalyzer: false,
-      analyzerMode: 'static',
-      reportFilename: path.join(dropPath, `${lastDirName}.stats.html`),
-      generateStatsFile: true,
-      statsFilename: path.join(dropPath, `${lastDirName}.stats.json`),
-      logLevel: 'error'
-    }));
+    additionalConfiguration: (generatedConfiguration) => {
+        const lastDirName = path.basename(__dirname);
+        const dropPath = path.join(__dirname, 'temp', 'stats');
+        generatedConfiguration.plugins.push(new bundleAnalyzer.BundleAnalyzerPlugin({
+            openAnalyzer: false,
+            analyzerMode: 'static',
+            reportFilename: path.join(dropPath, `${lastDirName}.stats.html`),
+            generateStatsFile: false,
+            logLevel: 'error'
+        }));
 
-    return generatedConfiguration;
-  }
+        return generatedConfiguration;
+    }
 });
 
 
