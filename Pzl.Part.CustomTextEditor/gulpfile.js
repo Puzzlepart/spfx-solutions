406c87aef72164b91f9176ac6aa42c9b20385479
Peter Holme Obrestad <peter.holme.obrestad@nav.no>
2021-11-23 13:55:29 +0100

    upgrade spfx

--- Pzl.Part.CustomTextEditor/gulpfile.js
+++ Pzl.Part.CustomTextEditor/gulpfile.js
@@ -1,36 +1,16 @@
 'use strict';
 
-const gulp = require('gulp');
-const path = require('path');
 const build = require('@microsoft/sp-build-web');
-const bundleAnalyzer = require('webpack-bundle-analyzer');
 
-build.addSuppression(/Warning/gi);
+build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
-build.configureWebpack.mergeConfig({
-    additionalConfiguration: (generatedConfiguration) => {
-        const lastDirName = path.basename(__dirname);
-        const dropPath = path.join(__dirname, 'temp', 'stats');
-        generatedConfiguration.plugins.push(new bundleAnalyzer.BundleAnalyzerPlugin({
-            openAnalyzer: false,
-            analyzerMode: 'static',
-            reportFilename: path.join(dropPath, `${lastDirName}.stats.html`),
-            generateStatsFile: true,
-            statsFilename: path.join(dropPath, `${lastDirName}.stats.json`),
-            logLevel: 'error'
-        }));
+var getTasks = build.rig.getTasks;
+build.rig.getTasks = function () {
+  var result = getTasks.call(build.rig);
 
-        return generatedConfiguration;
-    }
-});
+  result.set('serve', result.get('serve-deprecated'));
 
-let copyDynamic = build.subTask('copy-tinymce-theme', function (gulp, buildOptions, done) {
-    gulp.src('./assets/**')
-        .pipe(gulp.dest('./temp/deploy'))
-        .pipe(gulp.dest('./dist'));
+  return result;
+};
 
-    done();
-});
-build.rig.addPostBuildTask(copyDynamic);
-
-build.initialize(gulp);
+build.initialize(require('gulp'));
