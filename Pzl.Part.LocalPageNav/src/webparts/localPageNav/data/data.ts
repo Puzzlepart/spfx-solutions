e79ab6f3611d6264cbda58306420557350791e3b
Petter Skodvin-Hvammen <petter.skodvin@gmail.com>
2022-05-11 13:24:30 +0200

    Fixed duplicate headings

--- Pzl.Part.LocalPageNav/src/webparts/localPageNav/data/data.ts
+++ Pzl.Part.LocalPageNav/src/webparts/localPageNav/data/data.ts
@@ -8,57 +8,48 @@ import { includes } from 'lodash';
  * @returns Navigation links grouped by their position on the page.
  */
 export const getNavLinks = (selector: string[]): INavLinkGroup => {
-    let navLinks: INavLink[] = [];
-    const nodes: NodeList = document.querySelectorAll(selector.join(','));
-    let currentHeader: INavLink;
-    let prevLink: INavLink;
-    let currentSubheader: INavLink;
-    let prevPosition: number;
-    nodes.forEach((node: any, key, parent) => {
-        if (node.id !== '') {
-            const currentPosition: number = parseInt(node.localName.substring(1));
-            const navLink: INavLink = { name: node.innerText, key: `#${node.id}`, url: `#${node.id}`, links: [], isExpanded: true, linkStyle: currentPosition };
-            if (prevPosition) {
-                if (currentPosition > prevPosition) {
-                    if (!currentHeader) {
-                        currentHeader = prevLink;
-                        currentHeader.links.push(navLink);
-                    } else if (currentSubheader) {
-                        currentSubheader.link.push(navLink);
-                    } else {
-                        if (prevLink !== currentHeader) {
-                            prevLink.links.push(navLink);
-                            if (!currentSubheader) currentSubheader = prevLink;
-                        } else currentHeader.links.push(navLink);
-                    }
-                }
-                if (currentPosition < prevPosition) {
-                    if (currentSubheader && !includes(currentHeader.links, currentSubheader)) currentHeader.links.push(currentSubheader);
-                    if (navLink.linkStyle > currentHeader.linkStyle) {
-                        currentHeader.links.push(navLink);
-                    } else {
-                        navLinks.push(currentHeader);
-                        currentHeader = navLink;
-                    }
-                    currentSubheader = null;
-                }
-                if (currentPosition === prevPosition) {
-                    if (currentSubheader && currentSubheader.linkStyle < currentPosition) {
-                        currentSubheader.links.push(navLink);
-                    } else if (currentHeader && currentHeader.linkStyle < currentPosition) {
-                        currentHeader.links.push(navLink);
-                    } else {
-                        const links: INavLink[] = includes(navLinks, prevLink) ? [navLink] : [prevLink, navLink];
-                        navLinks.push(...links);
-                    }
-                }
-            }
-            prevPosition = currentPosition;
-            prevLink = navLink;
-        }
-        if (parent.length === key + 1 && currentHeader && !includes(navLinks, currentHeader)) {
-            navLinks.push(currentHeader);
-        }
+    let map = {}, links = [];
+
+    const nodes: any = Array.from<any>(document.querySelectorAll(selector.join(','))).map(node => {
+        return {
+            name: node.innerText,
+            key: `#${node.id}`,
+            url: `#${node.id}`,
+            links: [],
+            isExpanded: true,
+            linkStyle: parseInt(node.localName.substring(1)) // The level
+        };
     });
-    return { links: navLinks };
+
+    for (let i = 0; i < nodes.length; i++) {
+
+        map[nodes[i].key] = i; // Initialize the map
+
+        if (i === 0 || nodes[i].linkStyle === 1) {
+            nodes[i].parent = ''; // First node and level 1 nodes doesn't have a parent
+        } else if (nodes[i].linkStyle > nodes[i - 1].linkStyle) {
+            nodes[i].parent = nodes[i - 1].key; // Level 2 or 3, below previous, which is the parent
+        } else if (nodes[i].linkStyle === nodes[i - 1].linkStyle) {
+            nodes[i].parent = nodes[i - 1].parent; // Level 2 or 3, like the previous, share the parent
+        }
+        else {
+            // Level 2, previous was level 3. Need to traverse back to find parent
+            let p = i - 1;
+            while (nodes[p].linkStyle > nodes[i].linkStyle && 0 < p) {
+                p--;
+            }
+            nodes[i].parent = nodes[p].parent; // Use the parent of the first previous heading not on lower level
+        }
+    }
+
+    for (let i = 0; i < nodes.length; i++) {
+        let node = nodes[i];
+        if (node.parent !== '') {
+            // if you have dangling branches check that map[node.parent] exists
+            nodes[map[node.parent]].links.push(node);
+        } else {
+            links.push(node);
+        }
+    }
+    return { links: links };
 };
