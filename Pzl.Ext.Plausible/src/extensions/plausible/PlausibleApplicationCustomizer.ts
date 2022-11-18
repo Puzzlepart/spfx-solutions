32554dda18fe6d8a8eb66f68c89e4b3496a50a34
Petter Skodvin-Hvammen <petter.skodvin@gmail.com>
2022-11-18 11:21:13 +0100

    Plausible app customizer initial commit

---
+++ Pzl.Ext.Plausible/src/extensions/plausible/PlausibleApplicationCustomizer.ts
@@ -0,0 +1,39 @@
+import { Log } from '@microsoft/sp-core-library';
+import {
+  BaseApplicationCustomizer
+} from '@microsoft/sp-application-base';
+import { Dialog } from '@microsoft/sp-dialog';
+
+import * as strings from 'PlausibleApplicationCustomizerStrings';
+
+const LOG_SOURCE: string = 'PlausibleApplicationCustomizer';
+
+/**
+ * If your command set uses the ClientSideComponentProperties JSON input,
+ * it will be deserialized into the BaseExtension.properties object.
+ * You can define an interface to describe it.
+ */
+export interface IPlausibleApplicationCustomizerProperties {
+  // This is an example; replace with your own property
+  testMessage: string;
+}
+
+/** A Custom Action which can be run during execution of a Client Side Application */
+export default class PlausibleApplicationCustomizer
+  extends BaseApplicationCustomizer<IPlausibleApplicationCustomizerProperties> {
+
+  public onInit(): Promise<void> {
+    Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
+
+    let message: string = this.properties.testMessage;
+    if (!message) {
+      message = '(No properties were provided.)';
+    }
+
+    Dialog.alert(`Hello from ${strings.Title}:\n\n${message}`).catch(() => {
+      /* handle error */
+    });
+
+    return Promise.resolve();
+  }
+}
---
+++ Pzl.Ext.Plausible/src/extensions/plausible/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IPlausibleApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'PlausibleApplicationCustomizerStrings' {
+  const strings: IPlausibleApplicationCustomizerStrings;
+  export = strings;
+}
