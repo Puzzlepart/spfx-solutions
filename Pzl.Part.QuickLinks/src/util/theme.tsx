8b66f876e0480e50ef54a0d1a104c586bcf43fdf
Remi Blom-Ohlsen <remi.blom-ohlsen@puzzlepart.com>
2023-11-28 14:46:28 +0100

    Add theme util component

---
+++ Pzl.Part.QuickLinks/src/util/theme.tsx
@@ -0,0 +1,27 @@
+import { BrandVariants, createDarkTheme, createLightTheme } from '@fluentui/react-components'
+import pSBC from 'shade-blend-color'
+
+const themeColors: any = (window as any).__themeState__.theme
+const primaryColor = themeColors.themePrimary
+
+const brandVariants: BrandVariants = {
+  10: pSBC(-0.4, primaryColor),
+  20: pSBC(-0.35, primaryColor),
+  30: pSBC(-0.3, primaryColor),
+  40: pSBC(-0.25, primaryColor),
+  50: pSBC(-0.2, primaryColor),
+  60: pSBC(-0.15, primaryColor),
+  70: pSBC(-0.1, primaryColor),
+  80: pSBC(-0.05, primaryColor),
+  90: primaryColor,
+  100: pSBC(0.05, primaryColor),
+  110: pSBC(0.1, primaryColor),
+  120: pSBC(0.15, primaryColor),
+  130: pSBC(0.2, primaryColor),
+  140: pSBC(0.25, primaryColor),
+  150: pSBC(0.3, primaryColor),
+  160: pSBC(0.35, primaryColor)
+}
+
+export const customLightTheme = createLightTheme(brandVariants)
+export const customDarkTheme = createDarkTheme(brandVariants)
