87471566da2f1ab46c6d1b9479bf92ccd6eb0976
Remi Blom-Ohlsen <remi.blom-ohlsen@puzzlepart.com>
2023-12-13 09:53:55 +0100

    [Pzl.Part.QuickLinks] Adjustments to themeing

--- Pzl.Part.QuickLinks/src/util/theme.tsx
+++ Pzl.Part.QuickLinks/src/util/theme.tsx
@@ -5,14 +5,14 @@ const themeColors: any = (window as any).__themeState__.theme
 const primaryColor = themeColors.themePrimary
 
 const brandVariants: BrandVariants = {
-  10: pSBC(-0.4, primaryColor),
-  20: pSBC(-0.35, primaryColor),
-  30: pSBC(-0.3, primaryColor),
-  40: pSBC(-0.25, primaryColor),
-  50: pSBC(-0.2, primaryColor),
-  60: pSBC(-0.15, primaryColor),
-  70: pSBC(-0.1, primaryColor),
-  80: pSBC(-0.05, primaryColor),
+  10: pSBC(-0.5, primaryColor),
+  20: pSBC(-0.45, primaryColor),
+  30: pSBC(-0.4, primaryColor),
+  40: pSBC(-0.35, primaryColor),
+  50: pSBC(-0.3, primaryColor),
+  60: pSBC(-0.25, primaryColor),
+  70: pSBC(-0.2, primaryColor),
+  80: pSBC(-0.1, primaryColor),
   90: primaryColor,
   100: pSBC(0.05, primaryColor),
   110: pSBC(0.1, primaryColor),
